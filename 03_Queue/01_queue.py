9e3b0ef45073f206069bc6f51c41d418208a4f7e
venkat <venkatpselvam1@gmail.com>
2023-04-23 19:59:52 +0530

    Queue

---
+++ 03_Queue/01_queue.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, val, next):
+        self.val = val 
+        self.next = next
+    def insert_at_front(self, v):
+        return Node(v,self)
+    def delete_at_last(self):
+        dummy = self 
+        if self.next==None:
+            return None,self.val
+        while dummy.next.next!=None:
+            dummy=dummy.next
+        val=dummy.next.val
+        dummy.next=None
+        return self,val
+class Queue:
+    def __init__(self):
+        self.head=None
+    def enqueue(self,v):
+        if self.head==None:
+            self.head=Node(v,None)
+        else:
+            self.head=self.head.insert_at_front(v)
+    def dequeue(self):
+        if self.head==None:
+            print("Queue is empty")
+        a=self.head.delete_at_last()
+        self.head=a[0]
+        return a[1]
+    def isempty(self):
+        return self.head==None
+n=Queue()
+n.enqueue(2)
+n.enqueue(4)
+n.enqueue(4)
+a=n.dequeue()
+print(a)
+a=n.dequeue()
+print(a)
+b=n.isempty()
+print(b)
+a=n.dequeue()
+print(a)
+b=n.isempty()
+print(b)
